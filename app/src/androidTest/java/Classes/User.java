ad12c9aa4c6484c0a7d016cbfe4ade7585cd51f8
Marc <marc-malosse@hotmail.fr>
2017-11-24 16:21:25 +0100

    Ajout adresse

--- app/src/androidTest/java/Classes/User.java
+++ app/src/androidTest/java/Classes/User.java
@@ -8,11 +8,14 @@ public class User {
     private int id;
     private String prenom;
     private String telephone;
+    private String adresse;
 
-    public User(int id, String prenom, String telephone) {
+
+    public User(int id, String prenom, String telephone,String adresse) {
         this.id = id;
         this.prenom = prenom;
         this.telephone = telephone;
+        this.adresse = adresse;
     }
 
     public int getId() {
@@ -37,5 +40,13 @@ public class User {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
     }
 }
